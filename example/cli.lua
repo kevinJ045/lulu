dba21d882ae7d48637d6cdafb39f007fa07b99e6
makano <makanobush@gmail.com>
2025-10-15 05:35:27 +0300

    class decorators

--- example/cli.lua
+++ example/cli.lua
@@ -1,10 +1,22 @@
-class! @register() Example:Base(arg1, arg2), {
-  @log()
-  method(@validated x, y) {
-    print(x, y)
-  }
 
-  normal(x, y, z){
-    print(x, y, z)
+  class!
+  @States({
+    name = "something",
+    id = 1
+  })
+  @Component()
+  MyWidget:Widget(props),
+  {
+    @async()
+    @stated()
+    render(@Props({
+      name = self.name,
+      id = self.id
+    }) props, @List children) {
+      return lml! {
+        <box name={self.name} id={self.id}>
+          {children}
+        </box> 
+      }
+    }
   }
-}
